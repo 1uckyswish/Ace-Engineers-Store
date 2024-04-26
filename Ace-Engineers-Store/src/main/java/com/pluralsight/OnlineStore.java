6c60c600004183937af68b7d2718381f9398449f
Noel Guillen <noelblas274@gmail.com>
2024-04-25 23:50:50 -0400

    added starter code for project

---
+++ Ace-Engineers-Store/src/main/java/com/pluralsight/OnlineStore.java
@@ -0,0 +1,50 @@
+package com.pluralsight;
+
+public class OnlineStore {
+    public static void main(String[] args) {
+        System.out.println("Hello Tina");
+        System.out.println("Hello Staphon");
+        System.out.println("Hello Noel");
+    }
+}
+
+/*
+
+~~~~~~~~~~~~~ 10:20AM-10:25AM The Store Home Screen (T) ~~~~~~~~~~~~~~
+
+The home screen should display a list of options that a user can choose from.
+1 MIN (TINA-D) Display Products
+1 MIN (TINA) Display Cart
+1 MIN (TINA) Exit closes out of the application
+
+1~~~~~~~~~~~~~~~ 10:30AM-1:00PM Display Products (T/N)~~~~~~~~~~~~~
+
+10:30AM-10:40AM (TO DO) Create a Product class that stores all the properties defined in the csv file(N)
+10:40AM-10:55AM (TO DO) Use the products.csv file to load the store's product inventory into your application
+10:55AM-11:10AM (TO DO) Displays a list of products that your store sells.
+---- On this screen the customer should be able to Search or filter the list of products ----
+11:15AM-11:30AM (TO DO) - Product Name (N)
+11:30AM-11:45AM (TO DO) - Price (T)
+11:45AM-11:55AM (TO DO) - Department(T)
+LUNCH BREAK
+12:35PM-12:50PM (TO DO) Add a product to their cart (N)
+12:50PM-1:00PM (TO DO) - Go Back to the home page
+
+~~~~~~~~~~~~~ 1:00PM-2:30PM Display Cart (N) ~~~~~~~~~~~~~~~~~~
+
+1:05PM-1:15PM (TO DO) This displays a list of line items that are in the customer's cart.
+1:15PM-1:25PM (TO DO) It should also display the total sales amount of the cart.
+The customer should be able to:
+1:30PM-1:45PM (TO DO) Check Out
+1:45PM-2:00PM (TO DO) Remove Product from the cart
+1:45PM-2:00PM (TO DO) Go Back to the home screen
+2:10PM-2:30PM (TO DO) If customer chooses to remove a product need to prompt
+them for the product to remove
+
+~~~~~~~~~~~~~~~~~~~~~~~~~~~ 2:30-3:00 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+(TINA) TESTING
+(NOEL) !!! READ ME !!!
+(STAPHON) OPTIONAL TESTING
+
+*/
